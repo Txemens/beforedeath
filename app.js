9f10c42cb6933794256bfc83f325b9f188c8c45d
Txemens <mascarantitxema@gmail.com>
2017-10-17 12:45:17 +0100

    bootstrap and mongo
    adding bootstrap and mongo
    mongodb connection too

--- app.js
+++ app.js
@@ -42,5 +42,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+var db = require('./db')
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
 });
-
+router.get('/login', function(req, res, next) {
+  res.render('login', {title: 'login'});
+});
 module.exports = router;
